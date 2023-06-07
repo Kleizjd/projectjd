2957fcb510e2d534b3de19dd9dac04d58772592d
kleizjd <jose.jdgo97@gmail.com>
2023-06-07 11:57:05 -0500

    routes

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -38,7 +38,7 @@
                         <!-- Authentication Links -->
                         @guest
                         <li class="nav-item">
-                            <a class="nav-link" href="{{ route('welcome') }}">Principal</a>
+                            <a class="nav-link" href="/">Principal</a>
                             
                         </li>
                             {{-- @if (Route::has('login'))
